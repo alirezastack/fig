eaa4548b7c6657c2bb3046f84772c6eba3c4fbf7
Ali <alireza.hosseini@zoodroom.com>
2019-07-16 09:22:56 +0430

    0.0.4 - Upgrade grpc libs

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ f.close()
 
 setup(
     name='fig',
-    version='0.0.3',
+    version='0.0.4',
     description='Frontier API Gateway',
     long_description=LONG_DESCRIPTION,
     long_description_content_type='text/markdown',
